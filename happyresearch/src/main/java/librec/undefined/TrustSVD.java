d33294ef8eade61a90786eef223757ad2298442d
guoguibing <guoguibing@gmail.com>
2014-02-24 16:05:30 +0800

    TrustSVD new implementation

--- happyresearch/src/main/java/librec/undefined/TrustSVD.java
+++ happyresearch/src/main/java/librec/undefined/TrustSVD.java
@@ -97,6 +97,134 @@ public class TrustSVD extends SocialRecommender {
 				double w_tu = Math.sqrt(tu.length);
 
 				// update factors
+				double bu = userBiases.get(u);
+				double sgd = euj + regU * bu;
+				userBiases.add(u, -lRate * sgd);
+
+				loss += regU * bu * bu;
+
+				double bj = itemBiases.get(j);
+				sgd = euj + regI * bj;
+				itemBiases.add(j, -lRate * sgd);
+
+				loss += regI * bj * bj;
+
+				double[] sum_ys = new double[numFactors];
+				for (int f = 0; f < numFactors; f++) {
+					double sum = 0;
+					for (int i : nu)
+						sum += Y.get(i, f);
+
+					sum_ys[f] = w_nu > 0 ? sum / w_nu : sum;
+				}
+
+				double[] sum_ts = new double[numFactors];
+				for (int f = 0; f < numFactors; f++) {
+					double sum = 0;
+					for (int v : tu)
+						sum += W.get(v, f);
+
+					sum_ts[f] = w_tu > 0 ? sum / w_tu : sum;
+				}
+
+				for (int f = 0; f < numFactors; f++) {
+					double puf = P.get(u, f);
+					double qjf = Q.get(j, f);
+
+					double delta_u = euj * qjf + regU * puf;
+					double delta_j = euj * (puf + sum_ys[f] + sum_ts[f]) + regI * qjf;
+
+					PS.add(u, f, delta_u);
+					Q.add(j, f, -lRate * delta_j);
+
+					loss += regU * puf * puf + regI * qjf * qjf;
+
+					for (int i : nu) {
+						double yif = Y.get(i, f);
+
+						double delta_y = euj * qjf / w_nu + regI * yif;
+						Y.add(i, f, -lRate * delta_y);
+
+						loss += regI * yif * yif;
+					}
+
+					// update wvf
+					for (int v : tu) {
+						double tvf = W.get(v, f);
+
+						double delta_t = euj * qjf / w_tu + regU * tvf;
+						WS.add(v, f, delta_t);
+
+						loss += regU * tvf * tvf;
+					}
+				}
+			}
+
+			for (MatrixEntry me : socialMatrix) {
+				int u = me.row();
+				int v = me.column();
+				double tuv = me.get();
+				if (tuv <= 0)
+					continue;
+
+				double pred = DenseMatrix.rowMult(P, u, W, v);
+				double eut = pred - tuv;
+
+				loss += regS * eut * eut;
+
+				double csgd = eut * regS;
+
+				for (int f = 0; f < numFactors; f++) {
+					PS.add(u, f, csgd * W.get(v, f) + regU * P.get(u, f));
+					WS.add(v, f, csgd * P.get(u, f) + regU * W.get(v, f));
+				}
+			}
+
+			P = P.add(PS.scale(-lRate));
+			W = W.add(WS.scale(-lRate));
+
+			errs *= 0.5;
+			loss *= 0.5;
+
+			if (isConverged(iter))
+				break;
+
+		}// end of training
+	}
+
+	protected void buildModel_backup() {
+		for (int iter = 1; iter <= maxIters; iter++) {
+
+			loss = 0;
+			errs = 0;
+
+			DenseMatrix PS = new DenseMatrix(numUsers, numFactors);
+			DenseMatrix WS = new DenseMatrix(numUsers, numFactors);
+
+			for (MatrixEntry me : trainMatrix) {
+
+				int u = me.row(); // user
+				int j = me.column(); // item
+
+				double ruj = me.get();
+				if (ruj <= 0.0)
+					continue;
+
+				double pred = predict(u, j);
+				double euj = pred - ruj;
+
+				errs += euj * euj;
+				loss += euj * euj;
+
+				SparseVector ur = trainMatrix.row(u);
+				int[] nu = ur.getIndex();
+				double w_nu = Math.sqrt(nu.length);
+
+				SparseVector ut = socialMatrix.row(u);
+				int[] tu = ut.getIndex();
+				double w_tu = Math.sqrt(tu.length);
+
+				// update factors
 				double reg_u = 1.0 / w_nu;
 				double reg_j = wlr_j.get(j);
 
